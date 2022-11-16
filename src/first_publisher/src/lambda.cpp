719565ac9ae57e0400e00e3a49cf042b2e669231
Tharun Puthanveettil <tharunv.pvit@yahoo.com>
2022-11-15 23:41:46 -0500

    Adding modified publisher subscriber implementation
    In publisher, added a server to change the published method real-time. Also, functionality to change the frequency and topic using parameters. The functionality to change the frequency is added to subscriber node too.

--- src/first_publisher/src/lambda.cpp
+++ src/first_publisher/src/lambda.cpp
@@ -1,52 +1,119 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-#include <chrono>
-#include <memory>
-#include <string>
-
+// Copyright (c) 2022 by Tharun V. Puthanveettil
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "custom_services/srv/pass_msg.hpp"
 
-using namespace std::chrono_literals;
+// parameter types
+using PARAMETER_EVENT  = std::shared_ptr<rclcpp::ParameterEventHandler>;
+using PARAMETER_HNADLE = std::shared_ptr<rclcpp::ParameterCallbackHandle>;
 
-/* This example creates a subclass of Node and uses a fancy C++11 lambda
- * function to shorten the callback syntax, at the expense of making the
- * code somewhat more difficult to understand at first glance. */
-
-class MinimalPublisher : public rclcpp::Node {
+/**
+ * @brief Sample class to demonstrate the use of Ros2 publisher and server node simultaneously
+ * 
+ */
+class MyFirstPubSev : public rclcpp::Node {
  public:
-  MinimalPublisher() : Node("minimal_publisher"), count_(0) {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    auto timer_callback = [this]() -> void {
-      auto message = std_msgs::msg::String();
-      message.data = "Howdy, terps! " + std::to_string(this->count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      this->publisher_->publish(message);
-    };
-    timer_ = this->create_wall_timer(500ms, timer_callback);
-  }
+    MyFirstPubSev() : Node("my_first_publisher"), count_(0) {
+        auto param_desc = rcl_interfaces::msg::ParameterDescriptor();
+        param_desc.description = "Set callback frequency.";
+        this->declare_parameter("freq",
+        2.0, param_desc);  // default =  2 hz
+        auto param = this->get_parameter("freq");
+        auto freq = param.get_parameter_value().get<std::float_t>();
+
+        auto param_desc2 = rcl_interfaces::msg::ParameterDescriptor();
+        param_desc2.description = "Set topic name.";
+        this->declare_parameter("topic_name",
+        "topic_old", param_desc2);  // default =  "topic_old"
+        auto param2 = this->get_parameter("topic_name");
+        auto topicname = param2.get_parameter_value().get<std::string>();
+
+        publisher_ = this->create_publisher<std_msgs::msg::String>(topicname,
+         10);
+        timer_ = this->create_wall_timer(std::chrono
+        ::milliseconds(static_cast<int> ((1000 / freq))),
+        std::bind(&MyFirstPubSev::publisher_timer_callback, this));
+        RCLCPP_INFO_STREAM(this->get_logger(), "Publisher has been started..");
+
+        server_ = this->create_service<custom_services::srv::PassMsg>(
+            "change_msg",
+            std::bind(&MyFirstPubSev::server_callback, this,
+            std::placeholders::_1, std::placeholders::_2));
+            RCLCPP_INFO_STREAM(this->get_logger(), "Server has been started..");
+    }
 
  private:
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Service<custom_services::srv::PassMsg>::SharedPtr server_;
+    std::string test_msg = "Hello USA!!";
+    int count_;
+
+    /**
+     * @brief Callback function for the publisher timer
+     * 
+     */
+    void publisher_timer_callback() {
+        auto message = std_msgs::msg::String();
+        message.data = test_msg;
+        count_++;
+        publisher_->publish(message);
+        RCLCPP_INFO(this->get_logger(), "I published: '%s'",
+         message.data.c_str());
+        if (count_ >= 5) {
+            RCLCPP_DEBUG_STREAM(this->get_logger(), "Tesing Logging(Debug)");
+        }
+        if (count_ >= 10) {
+            RCLCPP_INFO_STREAM(this->get_logger(), "Tesing Logging(Info)");
+        }
+        if (count_ >= 15) {
+            RCLCPP_WARN_STREAM(this->get_logger(), "Tesing Logging(Warn)");
+        }
+        if (count_ >= 20) {
+            RCLCPP_ERROR_STREAM(this->get_logger(), "Tesing Logging(Error)");
+        }
+        if (count_ >= 25) {
+            RCLCPP_FATAL_STREAM(this->get_logger(), "Tesing Logging(Fatal)");
+        }
+    }
+
+    /**
+     * @brief Callback function for the server
+     * 
+     * @param new_msg 
+     */
+    void change_string(std::string new_msg) {
+        test_msg = new_msg;
+    }
+
+    /**
+     * @brief Callback function for the server
+     * 
+     * @param request 
+     * @param response 
+     */ 
+    void server_callback(const std::shared_ptr<custom_services::
+    srv::PassMsg::Request> request,
+    std::shared_ptr<custom_services::srv::PassMsg::Response> response) {
+       response->output = request->msg;
+        change_string(request->msg);
+        RCLCPP_WARN_STREAM(this->get_logger(),
+         "Incoming request for changing message to" << request->msg);
+        }
 };
 
-int main(int argc, char* argv[]) {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
-  rclcpp::shutdown();
-  return 0;
+/**
+ * @brief Main function to run the node
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<MyFirstPubSev>();
+    // Create a shared pointer
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
--- src/first_subscriber/src/lambda.cpp
+++ src/first_subscriber/src/lambda.cpp
@@ -1,27 +1,29 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+// Copyright (c) 2022 by Tharun V. Puthanveettil
 #include <memory>
-
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
+
+// parameter types
+using PARAMETER_EVENT  = std::shared_ptr<rclcpp::ParameterEventHandler>;
+using PARAMETER_HNADLE = std::shared_ptr<rclcpp::ParameterCallbackHandle>;
+
+
+/**
+ * @brief Sample class to demonstrate the use of Ros2 subscriber
+ * 
+ */
 class MinimalSubscriber : public rclcpp::Node {
  public:
   MinimalSubscriber() : Node("minimal_subscriber") {
+    auto param_desc2 = rcl_interfaces::msg::ParameterDescriptor();
+    param_desc2.description = "Set topic name.";
+    this->declare_parameter("topic_name",
+    "topic_old", param_desc2);  // default =  "topic_old"
+    auto param2 = this->get_parameter("topic_name");
+    auto topicname = param2.get_parameter_value().get<std::string>();
     subscription_ = this->create_subscription<std_msgs::msg::String>(
-        "topic", 10, [this](std_msgs::msg::String::UniquePtr msg) {
+        topicname, 10, [this](std_msgs::msg::String::UniquePtr msg) {
           RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
         });
   }
@@ -30,6 +32,13 @@ class MinimalSubscriber : public rclcpp::Node {
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
+/**
+ * @brief Main function to run the subscriber node
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
 int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalSubscriber>());
