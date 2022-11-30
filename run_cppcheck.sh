d57ecaf2c6a54db4a29bf15e175ba6c2b213557e
Tharun Puthanveettil <tharunv.pvit@yahoo.com>
2022-11-30 13:01:26 -0500

    Adding modules for static analysis

--- run_cppcheck.sh
+++ run_cppcheck.sh
@@ -1,2 +1,2 @@
-
 cppcheck --enable=all --std=c++17 --suppress=missingIncludeSystem $( find . -name *.cpp | grep -vE -e "/src/first_publisher/src/*.cpp" -e "/src/first_subscriber/src/*.cpp") > results/cppcheck.txt
+
