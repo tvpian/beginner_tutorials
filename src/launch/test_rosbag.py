022973603ddca7b20d5acea93d928745ef5d59b8
Tharun Puthanveettil <tharunv.pvit@yahoo.com>
2022-11-30 12:58:00 -0500

    Adding launch file for ros2 bag
    Adding launch file for running rosbag with a enable parameter

---
+++ src/launch/test_rosbag.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import LaunchConfiguration, PythonExpression
+
+from launch_ros.actions import Node
+from launch.conditions import IfCondition
+
+
+def generate_launch_description():
+   return LaunchDescription([
+      DeclareLaunchArgument(
+            'enable_recording',
+            default_value='false'
+        ),
+    # record all topic (chatter, service_node)
+      ExecuteProcess(
+            condition=IfCondition(
+                PythonExpression([
+                    LaunchConfiguration('enable_recording')
+                ])
+            ),
+            cmd=[[
+                'rm -rf rosbag_recording/ && ros2 bag record -a -o rosbag_recording',
+            ]],
+            shell=True
+        ),
+    
+   ])
